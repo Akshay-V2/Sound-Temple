89b504a074803ce830ac336f0e2535e1e2e43a9d
Akshay V V <akshcodes2008@gmail.com>
2025-03-21 14:14:50 +0530

    fixed the 'flawed' volume system (although not perfect)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Sound from './components/Sound';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Sound from "./components/Sound";
 
 function App() {
   const [time, setTime] = useState(new Date());
@@ -9,60 +9,113 @@ function App() {
   useEffect(() => {
     const interval = setInterval(() => {
       setTime(new Date());
-      const formattedTime = time.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }).replace(/AM|PM/, '');
+      const formattedTime = time
+        .toLocaleTimeString("en-US", {
+          hour: "numeric",
+          minute: "numeric",
+          hour12: true,
+        })
+        .replace(/AM|PM/, "");
       setFormatTime(formattedTime);
 
-      if (time.getHours() >= 0) {
-        document.getElementById('am').classList.add('time-active');
-        document.getElementById('pm').classList.remove('time-active');
+      if (time.getHours() > 12) {
+        document.getElementById("pm").classList.add("time-active");
+        document.getElementById("am").classList.remove("time-active");
       } else {
-        document.getElementById('pm').classList.add('time-active');
-        document.getElementById('am').classList.remove('time-active');
+        document.getElementById("am").classList.add("time-active");
+        document.getElementById("pm").classList.remove("time-active");
       }
     }, 1000);
 
     return () => clearInterval(interval);
   }, []);
 
-const sounds = ["Rain", "Classroom", "Birds"];
-const [selectedSound, setSelectedSound] = useState(0);
-const [volume, setVolume] = useState(0);
+  const [sounds, setSounds] = useState([
+    { sound: "Rain", volume: 0 },
+    { sound: "Forest", volume: 0 },
+    { sound: "Ocean", volume: 0 },
+    { sound: "Fireplace", volume: 0 },
+    { sound: "Wind", volume: 0 },
+  ]);
+  const [selectedSound, setSelectedSound] = useState(0);
 
-useEffect(() => {
-  const handleKeyPress = (event) => {
-    if (event.key === 'ArrowUp') {
-      setSelectedSound(prevSound => (prevSound - 1 + sounds.length) % sounds.length);
-    } else if (event.key === 'ArrowDown') {
-      setSelectedSound(prevSound => (prevSound + 1) % sounds.length);
+  const updateVolume = (updateDirection) => {
+    const newArray = [...sounds];
+
+    if (updateDirection === "forward") {
+      newArray[selectedSound].volume = Math.min(
+        newArray[selectedSound].volume + 10,
+        100,
+      );
+    } else if (updateDirection === "backward") {
+      newArray[selectedSound].volume = Math.max(
+        newArray[selectedSound].volume - 10,
+        0,
+      );
     }
-  };
-  window.addEventListener('keydown', handleKeyPress);
 
-  return () => {
-    window.removeEventListener('keydown', handleKeyPress);
+    setSounds(newArray);
   };
-}, []);
+
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.key === "ArrowUp") {
+        setSelectedSound(
+          (prevSound) => (prevSound - 1 + sounds.length) % sounds.length,
+        );
+      } else if (event.key === "ArrowDown") {
+        setSelectedSound((prevSound) => (prevSound + 1) % sounds.length);
+      }
+    };
+    window.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleVolumeChange = (event) => {
+      if (event.key === "ArrowRight") {
+        updateVolume("forward");
+      } else if (event.key === "ArrowLeft") {
+        updateVolume("backward");
+      }
+    };
+    window.addEventListener("keydown", handleVolumeChange);
+    return () => {
+      window.removeEventListener("keydown", handleVolumeChange);
+    };
+  }, [selectedSound]);
 
   return (
     <>
-    <div className='time-wrapper'>
-      <div className='time'>{formatTime}</div>
+      <div className="time-wrapper">
+        <div className="time">{formatTime}</div>
 
-      <div className='ampm-wrapper'>
-        <div className='ampm' id='am'>AM</div>
-        <div className='ampm' id='pm'>PM</div>
-
+        <div className="ampm-wrapper">
+          <div className="ampm" id="am">
+            AM
+          </div>
+          <div className="ampm" id="pm">
+            PM
+          </div>
+        </div>
       </div>
-      
-    </div>
 
-    <div className='sounds-wrapper'>
-      {sounds.map((sound, index) => (
-        <Sound key={index} soundName={sound} isActive={index === selectedSound} isRunning={false}/>
-      ))}
-    </div>
+      <div className="sounds-wrapper">
+        {sounds.map((sound, index) => (
+          <Sound
+            key={index}
+            soundName={sound.sound}
+            isActive={index === selectedSound}
+            isRunning={sound.volume > 0 ? true : false}
+            volume={sound.volume}
+          />
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Sound.jsx
+++ src/components/Sound.jsx
@@ -1,40 +1,44 @@
 import React, { useEffect, useState } from "react";
 
 const Sound = (props) => {
-    const [volume, setVolume] = useState(0);
-
-    useEffect(() => {
-        const handleVolume = (event) => {
-            if (event.key == "ArrowRight") {
-                setVolume(prevVol => prevVol > 99 ? 100 : prevVol + 10);
-            } else if (event.key == "ArrowLeft") {
-                setVolume(prevVol => prevVol < 1 ? 0 : prevVol - 10);
+  return (
+    <>
+      <div className="sound-full">
+        <div className="sound-name-wrapper">
+          <img
+            className={
+              props.isRunning
+                ? "sound-indicator sound-indicator-active"
+                : "sound-indicator"
             }
-        }
-
-        window.addEventListener('keydown', handleVolume);
-
-        return () => {
-            window.removeEventListener('keydown', handleVolume);
-        }
-    }, [])
-    
-    return(
-        <>
-        <div className="sound-full">
-            <div className="sound-name-wrapper">
-                <img className={props.isRunning ? "sound-indicator sound-indicator-active" : "sound-indicator"} src="./active_indicator.svg"></img>
-                <div className={props.isActive ? "sound-name sound-active" : "sound-name"}>{props.soundName}</div>
-            </div>
-            <div className={props.isActive ? "volume-changer volume-changer-active" : "volume-changer"}>
-                <img className="volume-arrow" src="../public/arrow.svg"></img>
-                <div className="volume">{volume}%</div>
-                <img className="volume-arrow" src="../public/arrow.svg" id="volume-arrow-reverse"></img>
-            </div>
+            src="./active_indicator.svg"
+          ></img>
+          <div
+            className={
+              props.isActive ? "sound-name sound-active" : "sound-name"
+            }
+          >
+            {props.soundName}
+          </div>
         </div>
-
-        </>
-    );
-}
+        <div
+          className={
+            props.isActive
+              ? "volume-changer volume-changer-active"
+              : "volume-changer"
+          }
+        >
+          <img className="volume-arrow" src="../public/arrow.svg"></img>
+          <div className="volume">{props.volume}%</div>
+          <img
+            className="volume-arrow"
+            src="../public/arrow.svg"
+            id="volume-arrow-reverse"
+          ></img>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Sound;
