2a5ea359221a6f8919219270c71dd6e3a284060e
Akshay V V <akshcodes2008@gmail.com>
2025-03-23 20:49:02 +0530

    release of V1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Sound from "./components/Sound";
 
+import RainSound from "../public/sounds/rain.mp3";
+import ForestSound from "../public/sounds/forest.mp3";
+import OceanSound from "../public/sounds/ocean.mp3";
+import FireplaceSound from "../public/sounds/fireplace.mp3";
+import WindSound from "../public/sounds/wind.mp3";
+
 function App() {
   const [time, setTime] = useState(new Date());
   const [formatTime, setFormatTime] = useState(0);
@@ -191,10 +197,89 @@ function App() {
       window.removeEventListener("scroll", handleScroll);
       window.removeEventListener("wheel", handleWheel);
     };
-  }, [selectedSound]); // Keep selectedSound as a dependency
+  }, [selectedSound]);
+
+  const [rainAudio] = useState(new Audio(RainSound));
+  rainAudio.loop = true;
+  const [forestAudio] = useState(new Audio(ForestSound));
+  forestAudio.loop = true;
+  const [oceanAudio] = useState(new Audio(OceanSound));
+  oceanAudio.loop = true;
+  const [fireplaceAudio] = useState(new Audio(FireplaceSound));
+  fireplaceAudio.loop = true;
+  const [windAudio] = useState(new Audio(WindSound));
+  windAudio.loop = true;
+
+  useEffect(() => {
+    if (sounds[0].volume > 0) {
+      rainAudio.play();
+    } else {
+      rainAudio.pause();
+    }
+  }, [sounds]);
+
+  useEffect(() => {
+    if (sounds[1].volume > 0) {
+      forestAudio.play();
+    } else {
+      forestAudio.pause();
+    }
+  }, [sounds]);
+
+  useEffect(() => {
+    if (sounds[2].volume > 0) {
+      oceanAudio.play();
+    } else {
+      oceanAudio.pause();
+    }
+  }, [sounds]);
+
+  useEffect(() => {
+    if (sounds[3].volume > 0) {
+      fireplaceAudio.play();
+    } else {
+      fireplaceAudio.pause();
+    }
+  }, [sounds]);
+
+  useEffect(() => {
+    if (sounds[4].volume > 0) {
+      windAudio.play();
+    } else {
+      windAudio.pause();
+    }
+  }, [sounds]);
+
+  useEffect(() => {
+    rainAudio.volume = sounds[0].volume / 100;
+    forestAudio.volume = sounds[1].volume / 100;
+    oceanAudio.volume = sounds[2].volume / 100;
+    fireplaceAudio.volume = sounds[3].volume / 100;
+    windAudio.volume = sounds[4].volume / 100;
+  }, [sounds]);
+
+  const [isInitialized, setIsInitialized] = useState(false);
+
+  const initializeSounds = () => {
+    setIsInitialized(true);
+    rainAudio.play();
+    rainAudio.pause();
+  };
+
+  useEffect(() => {
+    const soundsWrapper = document.querySelector(".sounds-wrapper");
+    const style = "translate(0px, " + selectedSound * -24 + "px)";
+    soundsWrapper.style.transform = style;
+  }, [selectedSound]);
 
   return (
     <>
+      <div
+        className={isInitialized ? "initializer initialized" : "initializer"}
+        onClick={initializeSounds}
+      >
+        click to enter your own soundscape
+      </div>
       <div className="time-wrapper">
         <div className="time">{formatTime}</div>
 
